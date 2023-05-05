8af4758fea5047ed5d43a856ce0ba1de8ee17927
Ismail Barka <isbarka@e1r8p9.1337.ma>
2023-05-05 18:54:05 +0100

    stjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,32 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbadr <sbadr@student.42.fr>                +#+  +:+       +#+        */
+/*   By: isbarka <isbarka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 11:19:13 by sbadr             #+#    #+#             */
-/*   Updated: 2023/04/13 22:36:08 by sbadr            ###   ########.fr       */
+/*   Updated: 2023/05/05 18:52:02 by isbarka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static char	*ft_error(const char *s1, const char *s2)
+{
+	if (!s1 && !s2)
+		return (0);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	return (0);
+}
+
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*ptr;
-	size_t	i;
-	int		len;
+	int		i;
+	char	*to_return;
 
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	ptr = (char *)malloc(len + 1);
-	if (!ptr)
-		return (NULL);
 	i = 0;
-	while (ft_strlen(s1))
-		ptr[i++] = *s1++;
-	while (ft_strlen(s2))
-		ptr[i++] = *s2++;
-	ptr[i] = '\0';
-	return (ptr);
+	if (!s1 || !s2)
+		return (ft_error(s1, s2));
+	to_return = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!to_return)
+		return (0);
+	while (*s1 != '\0')
+	{
+		to_return[i] = *s1++;
+		i++;
+	}
+	while (*s2 != '\0')
+	{
+		to_return[i] = *s2++;
+		i++;
+	}
+	to_return[i] = '\0';
+	return (to_return);
 }
